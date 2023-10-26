c0c028f3fca8f433508c1bc75b4d723e5076600a
heman <hemangh@gmail.com>
2023-10-25 23:07:27 -0400

    test overlap name changes and additions

--- tests/test_Gaussian_overlap.f90
+++ tests/test_Gaussian_overlap.f90
@@ -1,11 +1,17 @@
 program test
     use Gaussian_overlap
+    use contracted_Gaussian_type, only : contrct_Gaussian
+    ! use basisfunctions, only: contrct_Gaussian
     implicit none
     
     real(8) :: alpha1, alpha2
     integer, dimension(3) :: pow1, pow2
     real(8), dimension(3) :: coord_1, coord_2
-    real(8) :: analytic_ans
+    real(8), allocatable  :: exps(:), coeff(:)
+    type(contrct_Gaussian) :: a, b
+
+    real(8) :: analytic_ans, norm_contr
+    integer :: i
     
     open(unit=7, file="test_log.txt")
 
@@ -81,6 +87,23 @@ program test
     analytic_ans = primitive_Gaussian_Overlap_Integral (alpha1, pow1, coord_1, alpha2, pow2, coord_2)
     write(7,'(A,Es22.15E2)' ) "px and px primitives, 0.1 bohr apart, alpha = 1:", analytic_ans
 
+    !! Specific tests:
+    !! s & s overlap, H2O tests:
+    allocate(exps(3), coeff(3))
+    exps = [130.709320, 23.808861, 6.44360830] 
+    coeff= [0.15432897, 0.53532814, 0.44463454]
+    pow1 = [0, 0, 0]
+    coord_1 = [0.000000000000 , -0.143225816552 ,  0.000000000000]
+    
+    call a%initialize(coord_1, pow1, exps, coeff) 
+    !! check each term for normalization:
+    do i = 1, size(exps)
+        analytic_ans = primitive_Gaussian_Overlap_Integral (exps(i),pow1,coord_1, exps(i),pow1,coord_1)
+        print*, "primitive ",i,":",analytic_ans * a%norm(i) ** 2
+    end do
+
+    norm_contr = contracted_Gaussian_overlap (a,a)  
+    print*, "contracted basis norm:", norm_contr 
     close(7)
     
 end program test
